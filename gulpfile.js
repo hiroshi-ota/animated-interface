de2d4ed3a930a541199756616f6971f2598424e7
Kamil Kaźmierowski <kamil.kazmierowski@gmail.com>
2016-02-02 17:29:26 +0000

    New main files for gulp
    package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
+
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  console.log('working');
+// modules
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
+var filesize = require('gulp-filesize');
+
+gulp.task('clean', function() {
+  return gulp.src('/build', {read: false})
+    .pipe(clean());
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('js/**/*.js')
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('build/scripts/'))
+});
+
+gulp.task('styles', function() {
+  return gulp.src('sass/**/*.scss')
+    .pipe(concat('main.css'))
+    .pipe(gulp.dest('build/styles/'))
 });
