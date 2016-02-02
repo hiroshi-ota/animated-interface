5f03446c7fc548ae54e16f6bc75959a5308f5fa6
Kamil Kaźmierowski <kamil.kazmierowski@gmail.com>
2016-02-02 23:24:13 +0000

    Setting Up Gulp
    - stream-series for concating files in order
    - gulp-if for development variable
    - gulp-compass and gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,43 @@
-
 var gulp = require('gulp');
+
+//settings
+var dev = true; //false to get distribution build
 
 // modules
 var clean = require('gulp-clean');
 var compass = require('gulp-compass');
 var concat = require('gulp-concat');
 var filesize = require('gulp-filesize');
-var compass = require('gulp-uglify');
+var uglify = require('gulp-uglify');
+var series = require('stream-series');
+var ify = require('gulp-if');
+var compass = require('gulp-compass');
+var sass = require('gulp-sass');
+var autoprefix = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
   return gulp.src('/build', {read: false})
     .pipe(clean());
 });
 
-gulp.task('scripts', function() {
-  return gulp.src('js/**/*.js')
+gulp.task('scripts', function () {
+  return series(gulp.src('js/vendor/*.js'), gulp.src('js/!(vendor)/*.js'))
     .pipe(concat('main.js'))
+    .pipe(ify(!dev, uglify()))
     .pipe(gulp.dest('build/scripts/'))
+    .pipe(filesize())
 });
 
-gulp.task('styles', function() {
+gulp.task('styles', function () {
   return gulp.src('sass/**/*.scss')
+    .pipe(sass())
+    .pipe(autoprefix('last 2 version', 'safari 5', 'ie 7', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe(concat('main.css'))
+    .pipe(ify(!dev, minifyCss()))
     .pipe(gulp.dest('build/styles/'))
+    .pipe(filesize())
 });
+
+gulp.task('build', ['clean', 'styles', 'scripts']);
+
---
+++ js/madules/jLetters.js
@@ -0,0 +1,99 @@
+(function ($) {
+  var jLetters = function (element, options) {
+    /*INITIALISATION*
+     //         - initialise jLetters
+     //         - set params & element
+     //         @element: element on which jLetters is working
+     //         @options: list of settings passed by user
+     //     */
+    var elem = $(element),
+      obj = this,
+      text = $(elem).text(),
+      params = $.extend({
+        precision: '',
+        animationDuration: 50,
+        animationSpeed: 1000,
+        separate: ' '
+      }, options || {});
+
+    //Auxiliary functions
+    var splitWords = function () {
+      var words = [];
+      words.push(text.split(' '));
+      return words[0];
+    };
+
+    var splitChars = function (word) {
+      var char = [];
+      if (typeof word !== 'string') {
+        word = text;
+      }
+      char.push(word.split(''));
+      return char[0];
+    };
+
+    var toSpan = function (array) {
+      var plain = '';
+      for (var ae in array) {
+        plain += '<span>' + array[ae] + '</span>';
+      }
+      $(elem)[0].innerHTML = plain;
+    };
+
+    var toSpanArray = function (array) {
+      var spans = [];
+      for (var elem in array) {
+        spans.push('<span>' + array[elem] + '</span>');
+      }
+      return spans;
+    };
+
+    var animate = function (child, index) {
+      setTimeout(
+        function () {
+          $(child).animate({
+            opacity: 1
+          }, params.animationSpeed)
+        }, params.animationDuration + (params.animationDuration * index)
+      )
+    };
+
+    //Show results
+    var show = function () {
+      params.precision === 'words' ?
+        toSpan(splitWords()) : toSpan(splitChars());
+
+      var children = $(elem).children();
+      $(children).each(function () {
+        $(this).css({
+          opacity: 0
+        });
+      });
+      for (var i = 0; i < children.length; i++) {
+        animate(children[i], i)
+      }
+    };
+    show();
+  };
+
+  $.fn.jLetters = function (options) {
+    return this.each(function () {
+      var element = $(this);
+      //Return early if element has a instance of jLetters
+      if (element.data('jLetters')) return;
+      var api = new jLetters(this, options);
+      //Store plugin object in this element's data
+      element.data('jLetters', api);
+    });
+
+  };
+
+  //Exception control:
+  function ExceptionLog(message) {
+    this.name = 'jLetters Error';
+    this.message = message;
+  }
+
+  ExceptionLog.prototype = Error.prototype;
+
+}(jQuery));
