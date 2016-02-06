cfb05433066b4be547eeeb47406e6e2f5788813f
Kamil Kaźmierowski <kamil.kazmierowski@gmail.com>
2016-02-06 17:19:38 +0000

    Added sub-menu annimation

--- js/modules/menu.js
+++ js/modules/menu.js
@@ -1,3 +1,31 @@
-function test() {
-  alert('dziala');
-}
+/*
+ *  menu header
+ */
+
+$(document).ready(function () {
+
+  var $subMenu = $('.sub');
+      $mainElem = $('.elem');
+
+
+  $mainElem.on('mouseover', function(elem){
+    var subMenu = $(this).find('.sub');
+    var elements = subMenu.find('.sub-elem');
+    elements.each(function(i, e){
+      setTimeout(function(){
+        $(e).stop().slideDown('normal');
+      }, i * 100);
+    });
+  });
+
+  $mainElem.on('mouseout', function(elem){
+    var subMenu = $(this).find('.sub');
+    var elements = subMenu.find('.sub-elem');
+    elements.each(function(i, e){
+      setTimeout(function(){
+        $(e).stop().slideUp('normal');
+      }, i * 100);
+    });
+  });
+
+}());
