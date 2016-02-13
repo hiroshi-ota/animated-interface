b853b7b5b25e92b334ae1489520b4eeccea50f60
Kamil Kaźmierowski <kamil.kazmierowski@gmail.com>
2016-02-13 22:39:20 +0000

    Work in progress

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -10514,29 +10514,84 @@ $(document).ready(function () {
 }());
 /*
  *  live-reloader
- *    :reloads page content without reloading the page
+ *    :reloader - reloads page content without reloading the page
+ *    :stateHelper - sets state of window.history
  */
 
 var reloader = (function () {
 
   var $reloader = $(document.getElementById('main-container')),
-    target = '',
-    links = $('.reloader-link'),
-    location = window.location.origin;
+      links = $('.reloader-link');
 
   var loadContent = function (target) {
     $reloader.animate({
       opacity: 0
-    }, 200, function() {
-      $reloader.load('..' + target);
-      $reloader.animate({opacity: 1});
-    })
+    }, 150, function() {
+      $reloader.load(target);
+      $reloader.animate({opacity: 1}, 300);
+    });
+  };
 
+  var setContext = function(target) {
+    document.title = target.text;
 
+    stateHelper.setActualState('..' + target.pathname);
+    console.log(window.history);
+  };
+
+  var refreshScripts = function() {
+    location.reload();
   };
 
   links.on('click', function (e) {
     e.preventDefault();
-    loadContent($(this)[0].pathname);
-  })
+
+    loadContent('..' + this.pathname);
+    setContext(this);
+  });
+
+  return {
+    loadContent: loadContent
+  }
+}());
+
+
+
+var stateHelper = (function() {
+
+  var states = {
+    prev: '',
+    act: ''
+  },
+      i = 0;
+
+  var setActualState = function(actual) {
+
+    var state = generateState();
+    window.history.pushState({"state": target.pathname + '.html'}, target.text, target.text + '.html');
+    //!!states.act ? states.prev = states.act : '';
+    //states.act = actual;
+  };
+
+  function getStates() {
+    return states;
+  }
+
+  function generateState() {
+    return 'state' + i;
+  }
+
+  $(window).on('hashchange', function() {
+    reloader.loadContent(states.prev);
+  });
+  //document.on('reloader-state-change', function() {
+  //  var state = document.getElementById('main-container');
+  //  setActualState(state.innerHTML);
+  //
+  //});
+
+  return {
+    historyStates: getStates,
+    setActualState: setActualState
+  }
 }());
