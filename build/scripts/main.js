a3c0d45dcd89b9e5e11756e839ae8948a5f14f4d
Kamil Kaźmierowski <kamil.kazmierowski@gmail.com>
2016-03-19 20:08:42 +0000

    one page containers // all css

--- build/scripts/main.js
+++ build/scripts/main.js
@@ -10865,76 +10865,75 @@ var isInViewport = (function () {
   });
 
 }());
-(function (window) {
-
-  'use strict';
-
-  var pluginName = 'pageModule',
-  defaults = {
-  };
-
-  function PageModule(element, options) {
-    element instanceof jQuery ?
-      this.element = element :
-      this.element = $(element);
-      this.parent = this.element.parent() || window;
-
-    this.settings = $.extend({}, defaults, options);
-    this.init();
-
-    this.remove = function() {
-      element.remove();
-      console.log('test');
-    }
-  }
-
-
-  PageModule.prototype = {
-
-    init: function () {
-      var that = this,
-          element = this.element;
-
-      that.setSize();
-    },
-
-    setSize: function() {
-      var that = this,
-          width = this.parent.innerWidth(),
-          height = this.parent.innerHeight();
-
-      //that.element.css({
-      //  width: width,
-      //  height: height
-      //})
-    },
-
-    remove: function() {
-      var that = this;
-
-      $(that).remove();
-    }
-  };
-
-  $.fn[pluginName] = function(options) {
-    this.each(function() {
-      if(!$.data(this, pluginName)) {
-        $.data(this, pluginName, new PageModule(this, options));
-      }
-    });
-    return this;
-  }
-}(window));
-
-$(document).ready(function() {
-  var home = $('#home-container').pageModule().css({backgroundColor: 'green'});
-  var modules = $('#modules-container').pageModule().css({backgroundColor: 'blue'});
-
-  console.log(home.data('pageModule'));
-  console.log(modules.data('pageModule'));
-
-});
-
+//(function (window) {
+//
+//  'use strict';
+//
+//  var pluginName = 'pageModule',
+//  defaults = {
+//  };
+//
+//  function PageModule(element, options) {
+//    element instanceof jQuery ?
+//      this.element = element :
+//      this.element = $(element);
+//      this.parent = this.element.parent() || window;
+//
+//    this.settings = $.extend({}, defaults, options);
+//    this.init();
+//
+//    this.remove = function() {
+//      element.remove();
+//      console.log('test');
+//    }
+//  }
+//
+//
+//  PageModule.prototype = {
+//
+//    init: function () {
+//      var that = this,
+//          element = this.element;
+//
+//      that.setSize();
+//    },
+//
+//    setSize: function() {
+//      var that = this,
+//          width = this.parent.innerWidth(),
+//          height = this.parent.innerHeight();
+//
+//      //that.element.css({
+//      //  width: width,
+//      //  height: height
+//      //})
+//    },
+//
+//    remove: function() {
+//      var that = this;
+//
+//      $(that).remove();
+//    }
+//  };
+//
+//  $.fn[pluginName] = function(options) {
+//    this.each(function() {
+//      if(!$.data(this, pluginName)) {
+//        $.data(this, pluginName, new PageModule(this, options));
+//      }
+//    });
+//    return this;
+//  }
+//}(window));
+//
+//$(document).ready(function() {
+//  var home = $('#home-container').pageModule().css({backgroundColor: 'green'});
+//  var modules = $('#modules-container').pageModule().css({backgroundColor: 'blue'});
+//
+//  console.log(home.data('pageModule'));
+//  console.log(modules.data('pageModule'));
+//
+//});
 
 
 /*
